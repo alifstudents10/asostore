9ddf74769452bb832045ea681ee3d4c86a3fa058
alifstudents10 <alifstudents10@gmail.com>
2025-07-23 22:43:42 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: process.env.VITE_BASE_PATH || "/asostore"
-  optimizeDeps: {
-    exclude: ['lucide-react'],
   },
 });
