421d44b4b8f6a9bf84650a02920bfc68feabb3ab
alifstudents10 <alifstudents10@gmail.com>
2025-07-28 13:58:02 +0530

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './AdminDashboard';
+import App from './AdminDashboard.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
